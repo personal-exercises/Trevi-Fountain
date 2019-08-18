a98499abfbe9be08ecf0ecd6f03fe95b1cd29ff8
宋晓彤 <1252418308@qq.com>
2019-08-18 22:31:54 +0800

    add the image of inde

---
+++ Frontend/src/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'index',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
